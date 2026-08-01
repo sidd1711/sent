3be1f3086d5a2a3aa77a78ca804feb68559f17aa
Sidharth Talekar <sidhaarthtalekar@gmail.com>
2026-08-01 09:02:39 +0530

    third commit

--- hello.php
+++ hello.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello, World!";
+echo "Hello,
 echo "This is a simple PHP script.";
 echo "It demonstrates basic PHP syntax and output.";
 echo "You can run this script on a web server with PHP installed.";
